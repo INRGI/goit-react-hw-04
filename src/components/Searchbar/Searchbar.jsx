a140b4d403799333c9cd90a5cb926eaa5b0ca634
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-25 10:41:13 +0100

    Fix double submit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,38 +2,39 @@ import { IoSearchSharp } from "react-icons/io5";
 import { useState } from 'react';
 import { Form, FormButton, Header, Input, Label } from './Searchbar.styled';
 
-const SearchBar = ({onSubmit}) => {
-    const [searchName, setSearchName] = useState('');
-    const [inputValue, setInputValue] = useState('');
+const SearchBar = ({ onSubmit }) => {
+  const [searchName, setSearchName] = useState('');
 
-    const handleChange = event => setInputValue(event.target.value);
+  const handleChange = event => setSearchName(event.target.value);
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        setSearchName(inputValue.trim());
-        onSubmit(searchName);
-        event.target.reset();
-    };
+  const handleSubmit = event => {
+    event.preventDefault();
+    const trimmedSearchName = searchName.trim();
+    setSearchName(trimmedSearchName);
+    onSubmit(trimmedSearchName);
+    event.target.reset();
+  };
 
-    return (
-          <Header>
-            <Form onSubmit={handleSubmit}>
-                    <FormButton type="submit">
-                    <IoSearchSharp size={24}/>
-                    <Label>Search</Label>
-                </FormButton>
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <FormButton type="submit">
+          <IoSearchSharp size={24}/>
+          <Label>Search</Label>
+        </FormButton>
 
-                <Input
-                    value={inputValue}
-                    name="searchName"
-                    type="text"
-                    autocomplete="off"
-                    placeholder="Search images and photos"
-                    onChange={handleChange}
-                />
-            </Form>
-        </Header>
-      );
+        <Input
+          value={searchName}
+          name="searchName"
+          type="text"
+          autoComplete="off"
+          placeholder="Search images and photos"
+          onChange={handleChange}
+        />
+      </Form>
+    </Header>
+  );
 };
 
 export default SearchBar;
+
